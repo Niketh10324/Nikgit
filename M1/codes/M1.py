8b13fbc78d5e5a26000f21bbb65cc83bcf2bafd5
Niketh10324 <achantanick@gmail.com>
2024-08-20 21:39:50 +0530

    MN1

---
+++ M1/codes/M1.py
@@ -0,0 +1,66 @@
+#Code by /sdcard/github/matgeo/codes/CoordGeoVV Sharma
+#September 12, 2023
+#Revised July 21, 2024
+#released under GNU GPL
+#Secion Formula
+
+
+import sys                                          #for path to external scripts
+sys.path.insert(0, '/home/niketh/matgeo/codes/CoordGeo')        #path to my scripts
+import numpy as np
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#local imports
+from line.funcs import *
+from triangle.funcs import *
+from conics.funcs import circ_gen
+
+
+
+
+#Given points
+A = np.array(([2,-2])).reshape(-1,1)
+B = np.array(([-7,4])).reshape(-1,1)
+
+
+#Point
+P1= (B+2*A)/(3)
+P2= (2*B+A)/(3) # calculating the coordinate points of R which divides the join between the two points
+print(P1,P2)
+
+#Generating all lines
+x_AB = line_gen(A,B)
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+
+#Labeling the coordinates
+tri_coords = np.block([[A,B,P1,P2]])
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','P1','P2']
+for i, txt in enumerate(vert_labels):
+    #plt.annotate(txt, # this is the text
+    plt.annotate(f'{txt}\n({tri_coords[0,i]:.0f}, {tri_coords[1,i]:.0f})',
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(20,-10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+# use set_position
+ax = plt.gca()
+#ax.spines['top'].set_color('none')
+#ax.spines['left'].set_position('zero')
+#ax.spines['right'].set_color('none')
+#ax.spines['bottom'].set_position('zero')
+ax.spines['left'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+#plt.xlabel('$x$')
+#plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+plt.savefig('fig1.png')
+plt.show()
